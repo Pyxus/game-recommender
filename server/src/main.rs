065083f8413287c67e670ee793ed6041e131457a
Ezekiel <ehalle4@students.towson.edu>
2023-04-19 18:18:52 -0400

    Added more game features to calculation

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,14 +3,14 @@ pub mod twitch;
 
 use bimap::{BiHashMap, BiMap};
 use dotenv::dotenv;
-use nalgebra::{DMatrix, DVector, RowDVector};
+use nalgebra::{Const, DMatrix, DVector, Dyn, Matrix, RowDVector, VecStorage};
 use nalgebra_sparse::{CooMatrix, CscMatrix};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fmt::Display;
-use twitch::igdb::{GameGenres, IGDBWrapper};
-
+use strum::{EnumCount, IntoEnumIterator};
+use twitch::igdb::{Genres, IGDBWrapper, PlayerPerspective, Themes};
 struct Client {
     id: String,
     secret: String,
@@ -24,8 +24,20 @@ struct RatedGame {
 
 #[derive(Clone, Serialize, Deserialize)]
 struct Game {
+    name: String,
     id: u64,
+    #[serde(default)]
     genres: Vec<u64>,
+    #[serde(default)]
+    themes: Vec<u64>,
+    #[serde(default)]
+    player_perspectives: Vec<u64>,
+}
+
+struct FeatureSet {
+    genres: HashSet<u64>,
+    themes: HashSet<u64>,
+    perspectives: HashSet<u64>,
 }
 
 impl Display for Game {
@@ -36,104 +48,71 @@ impl Display for Game {
 
 #[tokio::main]
 async fn main() {
+    // Init DB Wrapper
     let client = create_client();
     let mut igdb_wrapper = IGDBWrapper::new(client.id, client.secret);
     igdb_wrapper.refresh_auth().await;
 
+    // Generate input games for testing
     let rated_games = vec![
         RatedGame {
             game: Game {
+                name: String::from("Bloodborne"),
                 id: 7334,
                 genres: vec![12, 31],
-            }, // Bloodborne
+                themes: vec![1, 17, 19, 38],
+                player_perspectives: vec![2],
+            },
             rating: 2.0,
         },
         RatedGame {
             game: Game {
+                name: String::from("Elden Ring"),
                 id: 119133,
                 genres: vec![12, 31],
-            }, // Elden Ring
+                themes: vec![1, 17, 38],
+                player_perspectives: vec![2],
+            },
             rating: 1.0,
         },
         RatedGame {
             game: Game {
+                name: String::from("Guilty Gear Strive"),
                 id: 125764,
                 genres: vec![4],
-            }, // Strive
+                themes: vec![1],
+                player_perspectives: vec![4],
+            },
             rating: 1.5,
         },
     ];
+
+    // Generate candidate games using input game features
     let games = rated_games
         .iter()
         .map(|rg| rg.game.clone())
         .collect::<Vec<_>>();
-    let rating_mat =
-        DVector::from_iterator(rated_games.len(), rated_games.iter().map(|g| g.rating));
-    let feat_mat = create_feature_mat(&games).await;
-    let weighted_feat_mat = &feat_mat.transpose() * &rating_mat;
-    let user_profile = &weighted_feat_mat / weighted_feat_mat.sum();
-    let candidate_genres = vec![12, 31, 4];
-    let where_genre_str = format!(
-        "{}",
-        candidate_genres
-            .iter()
-            .map(|g| g.to_string())
-            .collect::<Vec<String>>()
-            .join(", ")
-    );
-    let candidate_games = igdb_wrapper
-        .query::<Vec<Game>>(
-            "games",
-            format!(
-                "
-            fields genres;
-            where genres = ({where_genre_str}) & rating > 6;
-            limit 500;
-            "
-            )
-            .as_str(),
-        )
-        .await
-        .expect("Failed to query database.");
-
+    let candidate_features = create_feature_set(&games);
+    let candidate_games = create_candidate_list(&igdb_wrapper, &games, &candidate_features).await;
     let candidate_mat = create_feature_mat(&candidate_games).await;
+
+    let user_profile = calc_profile_mat(&rated_games).await;
     let recomendation_list = &candidate_mat * &user_profile;
-    let mut rated_rec_list = Vec::<RatedGame>::new();
-    for (i, rating) in recomendation_list.column(0).iter().enumerate() {
-        rated_rec_list.push(RatedGame {
-            game: candidate_games[i].clone(),
-            rating: *rating,
-        })
-    }
-
-    rated_rec_list.sort_by(|a, b| a.rating.partial_cmp(&b.rating).unwrap());
-
-    let where_genre_str = format!(
-        "{}",
-        rated_rec_list
+    let mut recommended_games = Vec::from_iter(
+        recomendation_list
+            .column(0)
             .iter()
-            .map(|rg| rg.game.id.to_string())
-            .collect::<Vec<String>>()
-            .join(", ")
+            .enumerate()
+            .map(|(i, rating)| RatedGame {
+                game: candidate_games[i].clone(),
+                rating: *rating,
+            }),
     );
 
-    #[derive(Serialize, Deserialize)]
-    struct NamedGame {
-        id: u64,
-        name: String,
-    }
-
-    let recomendation_list = igdb_wrapper
-        .query::<Vec<NamedGame>>(
-            "games",
-            format!("fields name; where id = ({where_genre_str}) & follows > 2; limit 500;").as_str(),
-        )
-        .await
-        .expect("Failed to query database");
-    
-    for (i, game) in recomendation_list.iter().enumerate() {
-        println!("{} ({})", &game.name, &rated_rec_list[i].rating);
-    }
+    recommended_games.sort_by(|a, b| a.rating.partial_cmp(&b.rating).unwrap());
+    recommended_games
+        .iter()
+        .for_each(|rg| println!("{} ({})", rg.game.name, rg.rating));
 }
 
 fn create_client() -> Client {
@@ -144,275 +123,177 @@ fn create_client() -> Client {
     }
 }
 
-async fn create_feature_mat(games: &Vec<Game>) -> CscMatrix<f64> {
-    let genres = vec![
-        GameGenres::PointAndClick,
-        GameGenres::Fighting,
-        GameGenres::Shooter,
-        GameGenres::Music,
-        GameGenres::Platform,
-        GameGenres::Puzzle,
-        GameGenres::Racing,
-        GameGenres::RPG,
-        GameGenres::Simulator,
-        GameGenres::Sport,
-        GameGenres::Strategy,
-        GameGenres::TBS,
-        GameGenres::Tactical,
-        GameGenres::HackNSlash,
-        GameGenres::Trivia,
-        GameGenres::Pinball,
-        GameGenres::Adventure,
-        GameGenres::Indie,
-        GameGenres::Arcade,
-        GameGenres::VisualNovel,
-        GameGenres::CardAndBoardGame,
-        GameGenres::MOBA,
-    ];
+fn create_feature_set(games: &Vec<Game>) -> FeatureSet {
+    let genres = games
+        .iter()
+        .flat_map(|game| game.genres.iter())
+        .copied()
+        .collect();
 
-    let mut feat_mat = CooMatrix::<f64>::zeros(games.len(), genres.len());
+    let themes = games
+        .iter()
+        .flat_map(|game| game.themes.iter())
+        .copied()
+        .collect();
 
-    for (row, game) in games.iter().enumerate() {
-        for (col, genre) in genres.iter().enumerate() {
-            let genre_id = *genre as u64;
-            if game.genres.contains(&genre_id) {
-                feat_mat.push(row, col, 1.0);
-            }
-        }
-    }
+    let perspectives = games
+        .iter()
+        .flat_map(|game| game.player_perspectives.iter())
+        .copied()
+        .collect();
 
-    return CscMatrix::from(&feat_mat);
+    return FeatureSet {
+        genres,
+        themes,
+        perspectives,
+    };
 }
 
-async fn create_candidate_mat(db: &IGDBWrapper, genres: &Vec<u64>) -> CscMatrix<f64> {
-    let where_genre_str = format!(
+async fn create_candidate_list(
+    db: &IGDBWrapper,
+    input_games: &Vec<Game>,
+    feature_set: &FeatureSet,
+) -> Vec<Game> {
+    #[derive(Serialize, Deserialize)]
+    struct GameQuery {
+        id: u64,
+        similar_games: Vec<Game>,
+    }
+    let where_game_id_str = format!(
         "{}",
-        genres
+        input_games
             .iter()
-            .map(|g| g.to_string())
+            .map(|g| g.id.to_string())
             .collect::<Vec<String>>()
             .join(", ")
     );
 
-    let games = db
+    let q = db
+        .query::<Vec<GameQuery>>(
+            "games", 
+            format!(
+            "
+            fields similar_games.name, similar_games.genres, similar_games.themes, similar_games.player_perspectives;
+            where id = ({where_game_id_str});
+            limit 100; 
+            "
+            ).as_str()
+        )
+        .await
+        .expect("Failed to query database.");
+
+    let where_exlude_game_id = q
+        .iter()
+        .flat_map(|query| query.similar_games.iter())
+        .map(|game| game.id.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+
+    let where_genre_str = format!(
+        "{}",
+        feature_set
+            .genres
+            .iter()
+            .map(|g| (*g as u64).to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
+
+    let where_theme_str = format!(
+        "{}",
+        feature_set
+            .themes
+            .iter()
+            .map(|g| (*g as u64).to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
+
+    let where_perspective_str = format!(
+        "{}",
+        feature_set
+            .perspectives
+            .iter()
+            .map(|g| (*g as u64).to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
+
+    let similar_games = q
+        .iter()
+        .flat_map(|gq| gq.similar_games.clone())
+        .collect::<Vec<Game>>();
+
+    let mut candidate_games = db
         .query::<Vec<Game>>(
             "games",
             format!(
                 "
-            fields genres;
-            where genres = ({where_genre_str}) & rating > 6;;
-            sort release_dates.date desc;
-            limit 100;
+            fields name, genres, themes, player_perspectives;
+            where genres = 
+                ({where_genre_str}) 
+                & themes = ({where_theme_str}) 
+                & player_perspectives = ({where_perspective_str})
+                & id != ({where_exlude_game_id})
+                & rating > 6;
+            limit 500;
             "
             )
             .as_str(),
         )
         .await
         .expect("Failed to query database.");
+    
+    candidate_games.extend(similar_games);
 
-    return create_feature_mat(&games).await;
+    return candidate_games;
 }
 
-async fn get_genres_by_game(db: &IGDBWrapper, game_ids: &Vec<u64>) -> HashMap<u64, Vec<u64>> {
-    let where_genre_str = format!(
-        "{}",
-        game_ids
-            .iter()
-            .map(|gid| gid.to_string())
-            .collect::<Vec<String>>()
-            .join(", ")
-    );
+async fn create_feature_mat(games: &Vec<Game>) -> CscMatrix<f64> {
+    let total_features = Genres::COUNT + Themes::COUNT + PlayerPerspective::COUNT;
+    let mut feat_mat = CooMatrix::<f64>::zeros(games.len(), total_features);
+    let mut col = 0;
 
-    let query_result = db
-        .query::<Vec<Game>>(
-            "games",
-            format!(
-                "
-                fields genres;
-                where id = ({where_genre_str});
-                "
-            )
-            .as_str(),
-        )
-        .await
-        .expect("Query failed.")
-        .iter()
-        .map(|g| (g.id, g.genres.clone()))
-        .collect::<HashMap<_, _>>();
+    for (row, game) in games.iter().enumerate() {
+        for genre in Genres::iter() {
+            let genre_id = genre as u64;
+            if game.genres.contains(&genre_id) {
+                feat_mat.push(row, col, 1.0);
+            }
+            col += 1;
+        }
+        for theme in Themes::iter() {
+            let theme_id = theme as u64;
+            if game.themes.contains(&theme_id) {
+                feat_mat.push(row, col, 1.0);
+            }
+            col += 1;
+        }
+        for perspective in PlayerPerspective::iter() {
+            let perspective_id = perspective as u64;
+            if game.player_perspectives.contains(&perspective_id) {
+                feat_mat.push(row, col, 1.0);
+            }
+            col += 1;
+        }
+        col = 0;
+    }
 
-    return query_result;
+    return CscMatrix::from(&feat_mat);
 }
 
-/*
-async fn there_was_an_attempt() {
-    #[derive(Copy, Clone)]
-    struct RatedGame {
-        id: u64,
-        rating: f32,
-    }
-
-    dotenv().ok();
-    let client_id = env::var("TWITCH_CLIENT_ID").expect("Failed to get twitch client id.");
-    let client_secret =
-        env::var("TWITCH_CLIENT_SECRET").expect("Failed to get twitch client secret.");
-    let mut igdb_wrapper = igdb::IGDBWrapper::new(client_id, client_secret);
-    igdb_wrapper.refresh_auth().await;
-
-    let games = vec![
-        RatedGame {
-            id: 7334, // Bloodborne
-            rating: 1.0,
-        },
-        RatedGame {
-            id: 119133, // Elden Ring
-            rating: 1.0,
-        },
-        RatedGame {
-            id: 125764, // Strive
-            rating: 1.0,
-        },
-    ];
-
-    let genres = vec![
-        igdb::GameGenres::PointAndClick,
-        igdb::GameGenres::Fighting,
-        igdb::GameGenres::Shooter,
-        igdb::GameGenres::Music,
-        igdb::GameGenres::Platform,
-        igdb::GameGenres::Puzzle,
-        igdb::GameGenres::Racing,
-        igdb::GameGenres::RPG,
-        igdb::GameGenres::Simulator,
-        igdb::GameGenres::Sport,
-        igdb::GameGenres::Strategy,
-        igdb::GameGenres::TBS,
-        igdb::GameGenres::Tactical,
-        igdb::GameGenres::HackNSlash,
-        igdb::GameGenres::Trivia,
-        igdb::GameGenres::Pinball,
-        igdb::GameGenres::Adventure,
-        igdb::GameGenres::Indie,
-        igdb::GameGenres::Arcade,
-        igdb::GameGenres::VisualNovel,
-        igdb::GameGenres::CardAndBoardGame,
-        igdb::GameGenres::MOBA,
-    ];
-
-    #[derive(Serialize, Deserialize)]
-    struct GenreQuery {
-        id: u64,
-        genres: Vec<u64>,
-    }
-
-    let genres_by_game: HashMap<u64, Vec<u64>> = igdb_wrapper
-        .query::<Vec<GenreQuery>>(
-            "games",
-            "
-            fields genres;
-            where id = (7334, 119133, 125764);
-            ",
-        )
-        .await
-        .expect("Query failed.")
+async fn calc_profile_mat(
+    rated_games: &Vec<RatedGame>,
+) -> Matrix<f64, Dyn, Const<1>, VecStorage<f64, Dyn, Const<1>>> {
+    let games = rated_games
         .iter()
-        .map(|q| (q.id, q.genres.clone()))
-        .collect();
+        .map(|rg| rg.game.clone())
+        .collect::<Vec<_>>();
+    let user_rating_mat =
+        DVector::from_iterator(rated_games.len(), rated_games.iter().map(|g| g.rating));
+    let feat_mat = create_feature_mat(&games).await;
+    let weighted_feat_mat = feat_mat.transpose() * user_rating_mat;
+    let user_profile = &weighted_feat_mat / weighted_feat_mat.sum();
 
-    let row_to_game: BiHashMap<usize, u64> = games
-        .iter()
-        .enumerate()
-        .map(|(row, game)| (row, game.id))
-        .collect::<BiMap<usize, u64>>();
-
-    let col_to_genre: BiHashMap<usize, u64> = genres
-        .iter()
-        .enumerate()
-        .map(|(col, genre)| (col, *genre as u64))
-        .collect::<BiMap<usize, u64>>();
-
-    // Construct feature matrix
-    let mut feat_mat = CooMatrix::<f32>::new(games.len(), genres.len());
-    for row in 0..games.len() {
-        let game_id = row_to_game.get_by_left(&row).unwrap();
-        let genres = &genres_by_game[game_id];
-
-        for genre in genres {
-            let genre_id = *col_to_genre.get_by_right(&genre).unwrap();
-            feat_mat.push(row, genre_id, 1.0);
-        }
-    }
-    let feat_mat = CscMatrix::from(&feat_mat);
-
-    // Calcuate weighted feature matrix
-    let rating_vec = DVector::from_iterator(games.len(), games.iter().map(|g| g.rating));
-    let weighted_rating_mat = &feat_mat.transpose() * &rating_vec;
-
-    // Calcuate user profile (normalized sum of weighted feat matrix)
-    let mut user_profile = DVector::<f32>::zeros(genres.len());
-    let weighted_sums = weighted_rating_mat.column_sum();
-    let sum = weighted_sums.iter().sum::<f32>();
-    for col in 0..weighted_sums.ncols() {
-        user_profile[col] = weighted_sums[col] / sum;
-    }
-
-    // Multiply user profile matrix by candidate matrix. Result is a weighted candidate matrix.
-    let mut genre_ids: HashSet<u64> = std::collections::HashSet::new();
-    for genre_list in genres_by_game.values() {
-        for genre_id in genre_list {
-            genre_ids.insert(*genre_id);
-        }
-    }
-
-    // Construct candidate matrix
-    let where_genre_str = format!(
-        "({})",
-        genre_ids
-            .iter()
-            .map(|id| id.to_string())
-            .collect::<Vec<String>>()
-            .join(", ")
-    );
-
-    let candidate_games = igdb_wrapper
-        .query::<Vec<GenreQuery>>(
-            "games",
-            format!(
-                "
-                fields genres;
-                where genres = {where_genre_str} & release_dates.date > 2010 & rating > 5;
-                sort release_dates.date desc;
-                limit 100;
-                "
-            )
-            .as_str(),
-        )
-        .await
-        .expect("Query failed.");
-
-    let genres_by_game: HashMap<u64, Vec<u64>> = candidate_games
-        .iter()
-        .map(|q| (q.id, q.genres.clone()))
-        .collect();
-
-    let row_to_game: BiHashMap<usize, u64> = candidate_games
-        .iter()
-        .enumerate()
-        .map(|(row, game)| (row, game.id))
-        .collect::<BiMap<usize, u64>>();
-
-    let mut candidate_feat_mat = CooMatrix::<f32>::new(candidate_games.len(), genres.len());
-    for row in 0..games.len() {
-        let game_id = row_to_game.get_by_left(&row).unwrap();
-        let genres = &genres_by_game[game_id];
-
-        for genre in genres {
-            let genre_id = *col_to_genre.get_by_right(&genre).unwrap();
-            candidate_feat_mat.push(row, genre_id, 1.0);
-        }
-    }
-    let candidate_feat_mat = CscMatrix::from(&candidate_feat_mat);
-    let weighted_candidate_mat = &candidate_feat_mat * &user_profile;
-
-    // Sum weighted ratings and sort to rank
+    return user_profile;
 }
-*/
--- server/src/twitch.rs
+++ server/src/twitch.rs
@@ -2,33 +2,75 @@ pub mod igdb {
     use reqwest::Response;
     use serde::{de::DeserializeOwned, Deserialize, Serialize};
     use std::time::SystemTime;
+    use strum_macros::{EnumIter, EnumCount};
 
     #[repr(u64)]
-    #[derive(Copy, Clone)]
-    pub enum GameGenres {
-        Adventure = 31,
-        Arcade = 33,
-        CardAndBoardGame = 35,
-        Fighting = 4,
-        HackNSlash = 25,
-        Indie = 32,
-        MOBA = 36,
-        Music = 7,
-        Pinball = 30,
-        Platform = 8,
+    #[derive(Debug, Copy, Clone, EnumIter, EnumCount)]
+    pub enum Genres {
         PointAndClick = 2,
-        Puzzle = 9,
-        RPG = 12,
-        Racing = 10,
+        Fighting = 4,
         Shooter = 5,
+        Music = 7,
+        Platform = 8,
+        Puzzle = 9,
+        Racing = 10,
+        RTS = 11,
+        RPG = 12,
         Simulator = 13,
         Sport = 14,
         Strategy = 15,
         TBS = 16,
         Tactical = 24,
+        HackNSlash = 25,
         Trivia = 26,
+        Pinball = 30,
+        Adventure = 31,
+        Indie = 32,
+        Arcade = 33,
         VisualNovel = 34,
+        CardAndBoardGame = 35,
+        MOBA = 36,
     }
+
+    #[repr(u64)]
+    #[derive(Debug, Copy, Clone, EnumIter, EnumCount)]
+    pub enum Themes {
+        Action = 1,
+        Fantasy = 17,
+        SciFi = 18,
+        Horror = 19,
+        Thriller = 20,
+        Survival = 21,
+        Historical = 22,
+        Stealth = 23,
+        Comedy = 27,
+        Business = 28,
+        Drama = 31,
+        NonFiction = 32,
+        Sandbox = 33,
+        Educational = 34,
+        Kids = 35,
+        OpenWorld = 38,
+        Warfare = 39,
+        Party = 40,
+        FourX = 41,
+        Erotic = 42,
+        Mystery = 43,
+        Romance = 44,
+    }
+
+    #[repr(u64)]
+    #[derive(Debug, Copy, Clone, EnumIter, EnumCount)]
+    pub enum PlayerPerspective {
+        FirstPerson = 1,
+        ThirdPerson = 2,
+        Isometric = 3,
+        SideView = 4,
+        Text = 5,
+        Auditory = 6,
+        VR = 7,
+    }
+
     pub struct IGDBWrapper {
         auth: Auth,
         auth_refreshed_at: SystemTime,
